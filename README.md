134748d0a51cde633a2cb27306fbd3e06b6bcd89
Kyytox <kytox.dev@protonmail.com>
2023-07-22 02:43:04 +0200

    gitignore + schema pipeline

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Allows you to listen the new extracts of vinyls from the shops Reggae Dub
 
 New version with a data engineer approach
-Pipeline - Airflow - PostgreSQL - Flask - React
+Pipeline - Docker - Airflow - PostgreSQL - Flask - React
--- old_version/init_bd.py
+++ old_version/init_bd.py
@@ -6,6 +6,10 @@ connection = sqlite3.connect('vinyls_dub_scrap.db')
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
+with open('schema.sql') as f:
+    connection.executescript(f.read())
 
 connection.commit()
 connection.close()
+
+
