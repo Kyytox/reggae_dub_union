e6010b0861b402e86903e6babf256159e0a69cc9
Kyytox <kytox.dev@protonmail.com>
2023-10-03 20:48:30 +0200

    update vinyls Navbar, remove old_version

--- website/frontend/src/components/Navbar.jsx
+++ website/frontend/src/components/Navbar.jsx
@@ -2,24 +2,37 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
 import SearchBar from "./SearchBar";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import "./Navbar.css";
 
 function Navbar() {
     const { isLoggedIn, logout } = useContext(AuthContext);
 
     return (
-        <div className="navbar p-1 pr-10 pl-10 mb-3 w-full flex items-center justify-between">
-            <div className="navbar_logo">
+        <div className="navbar p-1 pr-10 pl-10 mb-3 w-full flex flex-wrap items-center justify-center md:justify-between">
+            <div className="navbar_logo flex justify-center w-full md:w-auto">
                 <Link to="/">
                     <img
-                        className="navbar_logo mr-4"
+                        className="navbar_logo mr-4 ml-3 md:ml-0"
                         src="/logo.png"
                         alt="logo"
                         width="50px"
                     />
                 </Link>
+                <a
+                    href="https://www.facebook.com/association.tunguska/"
+                    target="_blank"
+                    style={{
+                        textDecoration: "none",
+                        fontSize: "0.7em",
+                        display: "flex",
+                        alignItems: "center",
+                    }}
+                >
+                    Sponso by Tunguska Sound System
+                </a>
             </div>
-            <div className="navbar_items">
+            <div className="navbar_items flex flex-wrap items-center justify-center">
                 <Link to="/">Home</Link>
 
                 <SearchBar />
@@ -37,6 +50,12 @@ function Navbar() {
                         <Link to="/login">Login</Link>
                     </>
                 )}
+                <Link
+                    to="https://github.com/Kyytox/vinyls_dub_scrap"
+                    target="_blank"
+                >
+                    <GitHubIcon />
+                </Link>
             </div>
         </div>
     );
