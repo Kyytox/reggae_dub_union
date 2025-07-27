b94b47cd0c3f5eabd1c0df2f00307a925487ee1e
Kyytox <kytox.dev@protonmail.com>
2025-07-27 10:25:33 +0200

    script for create connection db

---
+++ airflow/dags/scripts/create_connection.sh
@@ -0,0 +1,7 @@
+airflow connections add con_reggae_dub_union_db \
+                --conn-type 'postgres' \
+                --conn-login 'root' \
+                --conn-password 'root' \
+                --conn-host 'localhost' \
+                --conn-port '5432' \
+                --conn-schema 'reggae_dub_union'
