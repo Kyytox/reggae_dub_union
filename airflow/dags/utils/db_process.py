552710fca6fb5124dc0dceb8564abe9718408aae
Kyytox <kytox.dev@protonmail.com>
2025-08-09 05:23:39 +0200

    mv functions to db_process

---
+++ airflow/dags/utils/db_process.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from utils.db_connect import db_connect_postgres
+
+
+def get_shops_from_db(conn) -> pd.DataFrame:
+    """
+    Get all shops from the database.
+
+    Args:
+        conn: Database connection object.
+
+    Returns:
+        pd.DataFrame: DataFrame containing all shops with columns (name_shop, name_function, links).
+    """
+
+    query = "SELECT * FROM shops"
+    df = pd.read_sql(query, conn)
+
+    return df
+
+
+def get_shop_infos(conn, name_shop: str) -> pd.DataFrame:
+    """
+    Get links and infos for a specific shop from the database.
+
+    Args:
+        conn: Database connection object.
+        name_shop (str): Name of the shop to retrieve links for.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the shop's information and associated vinyls.
+    """
+
+    query = """
+        SELECT
+            s.shop_id,
+            s.shop_name,
+            sl.shop_link,
+            sl.shop_link_id,
+            v.vinyl_id,
+            v.vinyl_reference
+        FROM shops s
+        LEFT JOIN shops_links sl ON s.shop_id = sl.shop_id
+        LEFT JOIN vinyls v ON sl.shop_link_id = v.shop_link_id
+        WHERE
+            s.shop_name = %s
+            AND v.vinyl_id = (
+                SELECT MAX(v2.vinyl_id)
+                FROM vinyls v2
+                WHERE v2.shop_link_id = v.shop_link_id
+    )    """
+    # AND v.shop_link_id = sl.shop_link_id
+
+    df = pd.read_sql(query, conn, params=(name_shop,))
+
+    return df
