7115b4e7f173ae6958a1fa227dac40169535f5e1
Kyytox <kytox.dev@protonmail.com>
2025-08-09 05:24:30 +0200

    update tablas and insert

--- airflow/dags/sql/insert_shops.sql
+++ airflow/dags/sql/insert_shops.sql
@@ -1,50 +1,51 @@
 INSERT INTO public.shops (shop_id, shop_name, shop_function)
 VALUES
-(1, 'jahwaggysrecords', 'scrap_jahwaggysrecords'),
-(2, 'onlyrootsreggae', 'scrap_onlyrootsreggae'),
-(3, 'controltower', 'scrap_controltower'),
-(4, 'reggaefever', 'scrap_reggaefever'),
-(5, 'pataterecords', 'scrap_pataterecords'),
-(6, 'toolboxrecords', 'scrap_toolboxrecords'),
-(7, 'lionvibes', 'scrap_lionvibes'),
-(8, 'reggaemuseum', 'scrap_reggaemuseum');
+(1, 'jahwaggysrecords', 'scrap_jahwaggysrecords');
+-- (2, 'onlyrootsreggae', 'scrap_onlyrootsreggae'),
+-- (3, 'controltower', 'scrap_controltower'),
+-- (4, 'reggaefever', 'scrap_reggaefever'),
+-- (5, 'pataterecords', 'scrap_pataterecords'),
+-- (6, 'lionvibes', 'scrap_lionvibes');
+-- (7, 'toolboxrecords', 'scrap_toolboxrecords'),
+-- (8, 'reggaemuseum', 'scrap_reggaemuseum');
 
 
 INSERT INTO public.shops_links (shop_id, shop_link)
 VALUES
 (1, 'https://jahwaggysrecords.com/fr/5-brand-new-7-vinyl-selection?page='),
 (1, 'https://jahwaggysrecords.com/fr/6-brand-new-10-vinyl-selection?page='),
-(1, 'https://jahwaggysrecords.com/fr/18-test-press-selection?page='),
--- onlyrootsreggae links
-(2, 'https://www.onlyroots-reggae.com/fr/21-singles-7-45t/s-1/?page=X&order=product.date_add.desc'),
-(2, 'https://www.onlyroots-reggae.com/fr/20-maxis-12-10/s-1/?page=X&order=product.date_add.desc'),
-(2, 'https://www.onlyroots-reggae.com/fr/17-albums-lp-33t/s-1/?page=X&order=product.date_add.desc'),
--- controltower links
-(3, 'https://controltower.fr/fr/'),
--- reggaefever links
-(4, 'https://www.reggaefever.ch/articleList?format=7&style=Dub&sort=relDate_riddim&page='),
-(4, 'https://www.reggaefever.ch/articleList?format=7&style=Dancehall&sort=relDate_riddim&page='),
-(4, 'https://www.reggaefever.ch/articleList?format=7&style=Dub+Poetry&sort=relDate_riddim&page='),
-(4, 'https://www.reggaefever.ch/articleList?format=7&style=Reggae&sort=relDate_riddim&page='),
-(4, 'https://www.reggaefever.ch/articleList?format=7&style=Steppers&sort=relDate_riddim&page='),
-(4, 'https://www.reggaefever.ch/articleList?format=7&style=Ska%2BRocksteady&sort=relDate_riddim&page='),
--- ('https://www.reggaefever.ch/catalog?format=7&sort=relDate_riddim&page='),
-(4, 'https://www.reggaefever.ch/articleList?genKind=keyword&generic=&format=10&style=&sort=relDate_riddim&page='),
-(4, 'https://www.reggaefever.ch/articleList?genKind=keyword&generic=&format=12&style=&sort=relDate_riddim&page='),
-(4, 'https://www.reggaefever.ch/articleList?genKind=keyword&generic=&format=LP&style=&sort=relDate_riddim&page='),
--- pataterecords links
-(5, 'https://www.patate-records.com/shop/1/X/type/1/'),
-(5, 'https://www.patate-records.com/shop/1/X/type/2/'),
-(5, 'https://www.patate-records.com/shop/1/X/type/3/'),
+(1, 'https://jahwaggysrecords.com/fr/7-brand-new-12-vinyl-selection?page='),
+(1, 'https://jahwaggysrecords.com/fr/18-test-press-selection?page=');
+-- -- onlyrootsreggae links
+-- (2, 'https://www.onlyroots-reggae.com/fr/21-singles-7-45t/s-1/?page=X&order=product.date_add.desc'),
+-- (2, 'https://www.onlyroots-reggae.com/fr/20-maxis-12-10/s-1/?page=X&order=product.date_add.desc'),
+-- (2, 'https://www.onlyroots-reggae.com/fr/17-albums-lp-33t/s-1/?page=X&order=product.date_add.desc'),
+-- -- controltower links
+-- (3, 'https://controltower.fr/fr/'),
+-- -- reggaefever links
+-- (4, 'https://www.reggaefever.ch/articleList?format=7&style=Dub&sort=relDate_riddim&page='),
+-- (4, 'https://www.reggaefever.ch/articleList?format=7&style=Dancehall&sort=relDate_riddim&page='),
+-- (4, 'https://www.reggaefever.ch/articleList?format=7&style=Dub+Poetry&sort=relDate_riddim&page='),
+-- (4, 'https://www.reggaefever.ch/articleList?format=7&style=Reggae&sort=relDate_riddim&page='),
+-- (4, 'https://www.reggaefever.ch/articleList?format=7&style=Steppers&sort=relDate_riddim&page='),
+-- (4, 'https://www.reggaefever.ch/articleList?format=7&style=Ska%2BRocksteady&sort=relDate_riddim&page='),
+-- -- ('https://www.reggaefever.ch/catalog?format=7&sort=relDate_riddim&page='),
+-- (4, 'https://www.reggaefever.ch/articleList?genKind=keyword&generic=&format=10&style=&sort=relDate_riddim&page='),
+-- (4, 'https://www.reggaefever.ch/articleList?genKind=keyword&generic=&format=12&style=&sort=relDate_riddim&page='),
+-- (4, 'https://www.reggaefever.ch/articleList?genKind=keyword&generic=&format=LP&style=&sort=relDate_riddim&page='),
+-- -- pataterecords links
+-- (5, 'https://www.patate-records.com/shop/1/X/type/1/'),
+-- (5, 'https://www.patate-records.com/shop/1/X/type/2/'),
+-- (5, 'https://www.patate-records.com/shop/1/X/type/3/'),
+-- -- lionvibes links
+-- (6, 'https://www.lionvibes.com/collections/new-records?filter.p.m.custom.format=7%22&page=X&sort_by=created-descending'),
+-- (6, 'https://www.lionvibes.com/collections/new-records?filter.p.m.custom.format=10%22&page=X&sort_by=created-descending'),
+-- (6, 'https://www.lionvibes.com/collections/new-records?filter.p.m.custom.format=12%22&page=X&sort_by=created-descending'),
+-- (6, 'https://www.lionvibes.com/collections/new-records?filter.p.m.custom.format=LP&page=X&sort_by=created-descending');
 -- toolboxrecords links
-(6, 'https://www.toolboxrecords.com/fr/catalog/3/dub-ragga/#page='),
--- lionvibes links
-(7, 'https://www.lionvibes.com/collections/new-records?filter.p.m.custom.format=7%22&page=X&sort_by=created-descending'),
-(7, 'https://www.lionvibes.com/collections/new-records?filter.p.m.custom.format=10%22&page=X&sort_by=created-descending'),
-(7, 'https://www.lionvibes.com/collections/new-records?filter.p.m.custom.format=12%22&page=X&sort_by=created-descending'),
-(7, 'https://www.lionvibes.com/collections/new-records?filter.p.m.custom.format=LP&page=X&sort_by=created-descending'),
--- reggaemuseum links
-(8, 'https://www.reggae-museum.com/shop/24-dancehall-new-roots#/page-X'),
-(8, 'https://www.reggae-museum.com/shop/26-rub-a-dub-early-digital#/page-X'),
-(8, 'https://www.reggae-museum.com/shop/27-ska-rocksteady-roots#/page-X'),
-(8, 'https://www.reggae-museum.com/shop/28-12-inch-records#/page-X');
+-- (7, 'https://www.toolboxrecords.com/fr/catalog/3/dub-ragga/#page='),
+-- -- reggaemuseum links
+-- (8, 'https://www.reggae-museum.com/shop/24-dancehall-new-roots#/page-X'),
+-- (8, 'https://www.reggae-museum.com/shop/26-rub-a-dub-early-digital#/page-X'),
+-- (8, 'https://www.reggae-museum.com/shop/27-ska-rocksteady-roots#/page-X'),
+-- (8, 'https://www.reggae-museum.com/shop/28-12-inch-records#/page-X');
--- airflow/dags/sql/sql_init.sql
+++ airflow/dags/sql/sql_init.sql
@@ -4,13 +4,14 @@ CREATE SCHEMA IF NOT EXISTS public;
 CREATE TABLE IF NOT EXISTS public.vinyls (
     vinyl_id serial,
     shop_id integer,
-    vinyl_format character varying(255),
-    vinyl_title character varying(255),
-    vinyl_image character varying(255),
+    shop_link_id integer,
+    vinyl_format character varying(20),
+    vinyl_title character varying(300),
+    vinyl_image character varying(300),
     vinyl_price integer,
-    vinyl_currency character varying(255),
-    vinyl_reference character varying(255),
-    vinyl_url character varying(255),
+    vinyl_currency character varying(5),
+    vinyl_reference character varying(300),
+    vinyl_link character varying(300),
     PRIMARY KEY (vinyl_id)
 );
 
@@ -27,15 +28,15 @@ CREATE TABLE IF NOT EXISTS public.favoris (
 CREATE TABLE IF NOT EXISTS public.songs (
     song_id serial,
     vinyl_id integer NOT NULL,
-    song_title character varying(255),
-    song_mp3 character varying(255),
+    song_title character varying(700),
+    song_mp3 character varying(300),
     PRIMARY KEY (song_id)
 );
 
 
 CREATE TABLE IF NOT EXISTS public.users (
     user_id serial,
-    user_name character varying(255),
+    user_name character varying(50),
     user_password character varying(255),
     PRIMARY KEY (user_id)
 );
@@ -43,16 +44,16 @@ CREATE TABLE IF NOT EXISTS public.users (
 
 CREATE TABLE IF NOT EXISTS public.shops (
     shop_id integer,
-    shop_name character varying(255),
-    shop_function character varying(255),
+    shop_name character varying(40),
+    shop_function character varying(40),
     PRIMARY KEY (shop_id)
 );
 
 CREATE TABLE IF NOT EXISTS public.shops_links (
-    id_link serial,
+    shop_link_id serial,
     shop_id integer,
-    shop_link character varying(255),
-    PRIMARY KEY (id_link)
+    shop_link character varying(300),
+    PRIMARY KEY (shop_link_id)
 );
 
 
@@ -63,7 +64,7 @@ ALTER TABLE public.favoris
 ADD CONSTRAINT fk_favoris_id_user_users_id FOREIGN KEY(user_id) REFERENCES public.users(user_id);
 
 alter table public.favoris
-aDD CONSTRAINT fk_favoris_id_vinyl_vinyls_id foreign key(vinyl_id) references public.vinyls(vinyl_id);
+ADD CONSTRAINT fk_favoris_id_vinyl_vinyls_id foreign key(vinyl_id) references public.vinyls(vinyl_id);
 
 ALTER TABLE public.songs
 ADD CONSTRAINT fk_songs_id_vinyl_vinyls_id FOREIGN KEY(vinyl_id) REFERENCES public.vinyls(vinyl_id);
@@ -73,3 +74,6 @@ ADD CONSTRAINT fk_vinyls_id_shop_shops_id FOREIGN KEY(shop_id) REFERENCES public
 
 ALTER TABLE public.shops_links
 ADD CONSTRAINT fk_shops_links_id_shop_shops_id FOREIGN KEY(shop_id) REFERENCES public.shops(shop_id);
+
+ALTER TABLE public.vinyls
+ADD CONSTRAINT fk_vinyls_id_link_shops_links_id FOREIGN KEY(shop_link_id) REFERENCES public.shops_links(shop_link_id);
