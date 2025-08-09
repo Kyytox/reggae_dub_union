65594ff4d1ca23073cf794aad9dbc1a762d7d25e
Kyytox <kytox.dev@protonmail.com>
2025-08-09 05:24:09 +0200

    add delete blob

--- airflow/dags/utils/utils_gcp_storage.py
+++ airflow/dags/utils/utils_gcp_storage.py
@@ -8,18 +8,15 @@ def upload_blob(bucket_name, df, destination_blob_name):
     Uploads a file to the bucket.
 
     Args:
-                bucket_name (str): The name of the bucket to upload to.
-                source_file_name (str): The path to the file to upload.
-                destination_blob_name (str): The name of the blob in the bucket.
+        bucket_name (str): The name of the bucket to upload to.
+        source_file_name (str): The path to the file to upload.
+        destination_blob_name (str): The name of the blob in the bucket.
     """
 
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
-    # Optional: set a generation-match precondition to avoid potential race conditions
-    # and data corruptions. The request to upload is aborted if the object's
-    # generation number does not match your precondition.
     generation_match_precondition = 0
 
     blob.upload_from_string(
@@ -30,27 +27,49 @@ def upload_blob(bucket_name, df, destination_blob_name):
 
 
 def download_blob_into_memory(bucket_name, blob_name):
-    """Downloads a blob into memory."""
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
+    """Downloads a blob into memory.
 
-    # The ID of your GCS object
-    # blob_name = "storage-object-name"
+    Args:
+        bucket_name (str): The name of the bucket to download from.
+        blob_name (str): The name of the blob to download.
+
+    Returns:
+        bytes: The contents of the blob as bytes.
+    """
 
     storage_client = storage.Client()
 
     bucket = storage_client.bucket(bucket_name)
 
-    # Construct a client side representation of a blob.
-    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
-    # any content from Google Cloud Storage. As we don't need additional data,
-    # using `Bucket.blob` is preferred here.
     blob = bucket.blob(blob_name)
     contents = blob.download_as_bytes()
 
     print(f"Blob {blob_name} downloaded from bucket {bucket_name}.")
 
     return contents
+
+
+def delete_blob(bucket_name, blob_name):
+    """
+    Deletes a blob from the bucket.
+
+    Args:
+        bucket_name (str): The name of the bucket to delete from.
+        blob_name (str): The name of the blob to delete.
+    """
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    generation_match_precondition = None
+
+    blob.reload()  # Fetch blob metadata to use in generation_match_precondition.
+    generation_match_precondition = blob.generation
+
+    blob.delete(if_generation_match=generation_match_precondition)
+
+    print(f"Blob {blob_name} deleted.")
 
 
 def list_blobs_with_prefix(bucket_name, prefix, start_string, delimiter=None):
