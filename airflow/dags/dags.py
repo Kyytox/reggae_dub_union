ccd5375ec9d2d628308c027296cccdd306e9c6b9
Kyytox <kytox.dev@protonmail.com>
2025-08-11 02:34:48 +0200

    update task group

--- airflow/dags/dags.py
+++ airflow/dags/dags.py
@@ -1,14 +1,13 @@
 import pandas as pd
 from datetime import datetime, timedelta
 
-from airflow.sdk import DAG, task, task_group
+from airflow.sdk import DAG, task, task_group, get_current_context
 from airflow.utils.task_group import TaskGroup
 
 # Operators
 # from airflow.providers.standard.operators.bash import BashOperator
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-
 
 from airflow.models import Variable
 
@@ -114,6 +113,8 @@ with DAG(
     dag.doc_md = """
     # DAG for Extracting, Transforming and Loading vinyls from shops
 
+        # df = df.json()
+        # return df
     ## Tasks
 
     ### Extract
@@ -131,20 +132,31 @@ with DAG(
 
     """
 
-    # @task
     def get_shop_list(conn_id: str):
         """
         Get the list of shops from the database.
 
         Args:
-            conn_id (str): Airflow connection ID for the PostgreSQL database.
+        conn_id (str): Airflow connection ID for the PostgreSQL database.
 
         Returns:
-            df (pd.DataFrame): DataFrame containing the shop list with columns (shop_name, scrap_function).
+        df (pd.DataFrame): DataFrame containing the shop list with columns (shop_name, scrap_function).
 
         """
         conn = db_connect_postgres(conn_id)
+        cur = conn.cursor()
+
+        # Check if Table shops exists
+        query = "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'shops');"
+        result = pd.read_sql(query, conn).iloc[0, 0]
+
+        if not result:
+            # raise ValueError("Table 'shops' does not exist in the database.")
+            return pd.DataFrame()
+
         df = get_shops_from_db(conn)
+        print(df)
+        cur.close()
         conn.close()
         return df
 
@@ -155,15 +167,12 @@ with DAG(
     conn_id = Variable.get("conn_id", default_var=CONNECTION_DB)
     bucket_name = Variable.get("bucket_name", default_var=BUCKET_NAME)
 
-    with TaskGroup("tg_scrap_shops") as tg_scrap_shops:
-        """
-        Task Group to scrap shops.
-        This task group iterates over the list of shops
-        and creates a PythonOperator for each shop to perform the scraping.
-        """
-        df_shops = get_shop_list(conn_id=conn_id)
+    # Create a task for each shop using PythonOperator
+    with TaskGroup("tsk_gp") as tsk_gp:
+        # Get the list of shops from the database
+        shop_list = get_shop_list(conn_id)
 
-        for index, row in df_shops.iterrows():
+        for index, row in shop_list.iterrows():
             name_shop = row["shop_name"]
             scrap_function = row["shop_function"]
 
@@ -178,29 +187,26 @@ with DAG(
                 },
             )
 
-    # # Task Transform Data
-    # tsk_transform_data = PythonOperator(
-    #     task_id="tsk_transform_data",
-    #     python_callable=transform_data,
-    #     op_kwargs={
-    #         "bucket_name": bucket_name,
-    #         "time_file_name": time_file_name,
-    #     },
-    # )
-    #
-    # # Task Load Data to DB
-    # tsk_load_data = PythonOperator(
-    #     task_id="tsk_load_data",
-    #     python_callable=load_data_to_db,
-    #     op_kwargs={
-    #         "bucket_name": bucket_name,
-    #         "time_file_name": time_file_name,
-    #         "conn_id": conn_id,
-    #     },
-    # )
-    #
-    # # Run all scrap tasks
-    # tg_scrap_shops >> tsk_transform_data >> tsk_load_data
-    tg_scrap_shops
-    # tsk_transform_data
-    # tsk_load_data
+    # Task Transform Data
+    tsk_transform_data = PythonOperator(
+        task_id="tsk_transform_data",
+        python_callable=transform_data,
+        op_kwargs={
+            "bucket_name": bucket_name,
+            "time_file_name": time_file_name,
+        },
+    )
+
+    # Task Load Data to DB
+    tsk_load_data = PythonOperator(
+        task_id="tsk_load_data",
+        python_callable=load_data_to_db,
+        op_kwargs={
+            "bucket_name": bucket_name,
+            "time_file_name": time_file_name,
+            "conn_id": conn_id,
+        },
+    )
+
+    # Run all scrap tasks
+    (tsk_gp >> tsk_transform_data >> tsk_load_data)
