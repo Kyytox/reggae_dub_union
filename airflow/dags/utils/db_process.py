ecacb026ed33f79fb2c1bf4eb235061bf4dbe047
Kyytox <kytox.dev@protonmail.com>
2025-08-11 02:34:18 +0200

    update find shop_link if vinyls table is empty

--- airflow/dags/utils/db_process.py
+++ airflow/dags/utils/db_process.py
@@ -32,25 +32,50 @@ def get_shop_infos(conn, name_shop: str) -> pd.DataFrame:
         pd.DataFrame: DataFrame containing the shop's information and associated vinyls.
     """
 
-    query = """
+    # Chcek if data exist in vinyls table
+    query = """SELECT COUNT(*) 
+            FROM vinyls 
+            WHERE shop_link_id IN (
+                SELECT shop_link_id 
+                FROM shops_links
+                WHERE shop_id = (
+                    SELECT shop_id 
+                    FROM shops
+                    WHERE shop_name = %s))"""
+    count = pd.read_sql(query, conn, params=(name_shop,)).iloc[0, 0]
+
+    if count == 0:
+        query = """
         SELECT
             s.shop_id,
             s.shop_name,
             sl.shop_link,
             sl.shop_link_id,
-            v.vinyl_id,
-            v.vinyl_reference
+            NULL AS vinyl_id,
+            NULL AS vinyl_reference
         FROM shops s
         LEFT JOIN shops_links sl ON s.shop_id = sl.shop_id
-        LEFT JOIN vinyls v ON sl.shop_link_id = v.shop_link_id
-        WHERE
-            s.shop_name = %s
-            AND v.vinyl_id = (
-                SELECT MAX(v2.vinyl_id)
-                FROM vinyls v2
-                WHERE v2.shop_link_id = v.shop_link_id
-    )    """
-    # AND v.shop_link_id = sl.shop_link_id
+        WHERE s.shop_name = %s
+        """
+    else:
+        query = """
+            SELECT
+                s.shop_id,
+                s.shop_name,
+                sl.shop_link,
+                sl.shop_link_id,
+                v.vinyl_id,
+                v.vinyl_reference
+            FROM shops s
+            LEFT JOIN shops_links sl ON s.shop_id = sl.shop_id
+            LEFT JOIN vinyls v ON sl.shop_link_id = v.shop_link_id
+            WHERE
+                s.shop_name = %s
+                AND v.vinyl_id = (
+                    SELECT MAX(v2.vinyl_id)
+                    FROM vinyls v2
+                    WHERE v2.shop_link_id = v.shop_link_id
+        )    """
 
     df = pd.read_sql(query, conn, params=(name_shop,))
 
