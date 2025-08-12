8dca75bf5a9b510b34549c88d800c986d48f4e7b
Kyytox <kytox.dev@protonmail.com>
2025-08-12 03:01:23 +0200

    add variables prefix

--- airflow/dags/utils/variables.py
+++ airflow/dags/utils/variables.py
@@ -1,1 +1,5 @@
+BUCKET_NAME = "reggae_dub_union_bucket"
+CONNECTION_DB = "con_reggae_dub_union_db"
+
 lst_formats_accepted = ["7", "10", "12", "LP", "TEST PRESS"]
+prefix_file = "ext"  # prefix for file extract from shop
