c498a1132926bea8533f95a4049ef82264783ac8
Kyytox <kytox.dev@protonmail.com>
2025-08-23 06:37:50 +0200

    mv page auth

--- website/frontend/src/pages/LoginPage.jsx
+++ website/frontend/src/pages/LoginPage.jsx
@@ -8,10 +8,10 @@ import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import IconButton from "@mui/material/IconButton";
 import WarningIcon from "@mui/icons-material/Warning";
-import { AuthContext } from "./AuthContext";
-import { postAxios } from "./UtilsAxios";
+import { AuthContext } from "../components/AuthContext";
+import { postAxios } from "../requests/UtilsAxios";
 
-function Login() {
+function LoginPage() {
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
@@ -91,77 +91,79 @@ function Login() {
   };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexWrap: "nowrap",
-        flexDirection: "column",
-        width: "25rem",
-        padding: "4rem",
-        paddingTop: "2rem",
-        marginTop: "5rem",
-        borderRadius: "1rem",
-        backgroundColor: "#18181b",
-      }}
-      id="box-Login"
-    >
-      <h2 className="text-xl font-bold" id="h2-Signup">
-        Login
-      </h2>
-      <TextField
-        required
-        autoFocus
-        id="username"
-        label="Username"
-        variant="standard"
-        sx={sxTextField}
-        onChange={(e) => setUsername(e.target.value)}
-        error={!!usernameError}
-        helperText={usernameError}
-      />
-      <TextField
-        required
-        id="password"
-        label="Password"
-        variant="standard"
-        sx={sxTextField}
-        type={showPassword ? "text" : "password"}
-        onChange={(e) => setPassword(e.target.value)}
-        error={!!passwordError}
-        helperText={passwordError}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          ),
+    <div className="main-content">
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "nowrap",
+          flexDirection: "column",
+          width: "25rem",
+          padding: "4rem",
+          paddingTop: "2rem",
+          marginTop: "5rem",
+          borderRadius: "1rem",
+          backgroundColor: "#18181b",
         }}
-      />
-      <Button variant="contained" onClick={handleSubmit} sx={{ mt: 5 }}>
-        Login
-      </Button>
+        id="box-Login"
+      >
+        <h2 className="text-xl font-bold" id="h2-Signup">
+          Login
+        </h2>
+        <TextField
+          required
+          autoFocus
+          id="username"
+          label="Username"
+          variant="standard"
+          sx={sxTextField}
+          onChange={(e) => setUsername(e.target.value)}
+          error={!!usernameError}
+          helperText={usernameError}
+        />
+        <TextField
+          required
+          id="password"
+          label="Password"
+          variant="standard"
+          sx={sxTextField}
+          type={showPassword ? "text" : "password"}
+          onChange={(e) => setPassword(e.target.value)}
+          error={!!passwordError}
+          helperText={passwordError}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button variant="contained" onClick={handleSubmit} sx={{ mt: 5 }}>
+          Login
+        </Button>
 
-      {ResponseError && (
-        <div className="text-black flex justify-center text-m mt-4 p-2 bg-red-400 rounded">
-          <WarningIcon />
-          {ResponseError}
-        </div>
-      )}
+        {ResponseError && (
+          <div className="text-black flex justify-center text-m mt-4 p-2 bg-red-400 rounded">
+            <WarningIcon />
+            {ResponseError}
+          </div>
+        )}
 
-      {ResponseSuccess && (
-        <div className="text-black flex justify-center text-m mt-4 p-2 bg-green-400 rounded">
-          {ResponseSuccess}
-        </div>
-      )}
-    </Box>
+        {ResponseSuccess && (
+          <div className="text-black flex justify-center text-m mt-4 p-2 bg-green-400 rounded">
+            {ResponseSuccess}
+          </div>
+        )}
+      </Box>
+    </div>
   );
 }
 
-export default Login;
+export default LoginPage;
--- website/frontend/src/pages/SignupPage.jsx
+++ website/frontend/src/pages/SignupPage.jsx
@@ -8,11 +8,12 @@ import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import IconButton from "@mui/material/IconButton";
 import WarningIcon from "@mui/icons-material/Warning";
 import Box from "@mui/material/Box";
-import { AuthContext } from "./AuthContext";
-import { postAxios } from "./UtilsAxios";
+
+import { AuthContext } from "../components/AuthContext";
+import { postAxios } from "../requests/UtilsAxios";
 import "../App.css";
 
-function Signup() {
+function SignupPage() {
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
@@ -127,115 +128,117 @@ function Signup() {
   };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexWrap: "nowrap",
-        flexDirection: "column",
-        width: "25rem",
-        padding: "4rem",
-        paddingTop: "2rem",
-        marginTop: "5rem",
-        // border: "1px solid black",
-        borderRadius: "1rem",
-        backgroundColor: "#18181b",
-      }}
-      id="box-Signup"
-    >
-      <h2 className="text-xl font-bold" id="h2-Signup">
-        Signup
-      </h2>
-      <TextField
-        required
-        id="email"
-        label="Email"
-        variant="standard"
-        sx={sxTextField}
-        onChange={(e) => setEmail(e.target.value)}
-        error={!!emailError}
-        helperText={emailError}
-      />
-
-      <TextField
-        required
-        autoFocus
-        id="username"
-        label="Username"
-        variant="standard"
-        sx={sxTextField}
-        onChange={(e) => setUsername(e.target.value)}
-        error={!!usernameError}
-        helperText={usernameError}
-      />
-
-      <TextField
-        required
-        id="password"
-        label="Password"
-        variant="standard"
-        sx={sxTextField}
-        type={showPassword ? "text" : "password"}
-        onChange={(e) => setPassword(e.target.value)}
-        error={!!passwordError}
-        helperText={passwordError}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          ),
+    <div className="main-content">
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "nowrap",
+          flexDirection: "column",
+          width: "25rem",
+          padding: "4rem",
+          paddingTop: "2rem",
+          marginTop: "5rem",
+          // border: "1px solid black",
+          borderRadius: "1rem",
+          backgroundColor: "#18181b",
         }}
-      />
-      <TextField
-        required
-        id="confirm-password"
-        label="Confirm Password"
-        variant="standard"
-        sx={sxTextField}
-        type={showConfirmPassword ? "text" : "password"}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-        error={!!confirmPasswordError}
-        helperText={confirmPasswordError}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowConfirmPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-      />
-      <Button variant="contained" onClick={handleSubmit} sx={{ mt: 5 }}>
-        Sign Up
-      </Button>
+        id="box-Signup"
+      >
+        <h2 className="text-xl font-bold" id="h2-Signup">
+          Signup
+        </h2>
+        <TextField
+          required
+          id="email"
+          label="Email"
+          variant="standard"
+          sx={sxTextField}
+          onChange={(e) => setEmail(e.target.value)}
+          error={!!emailError}
+          helperText={emailError}
+        />
 
-      {ResponseError && (
-        <div className="text-black flex justify-center text-m mt-4 p-2 bg-red-400 rounded">
-          <WarningIcon />
-          {ResponseError}
-        </div>
-      )}
+        <TextField
+          required
+          autoFocus
+          id="username"
+          label="Username"
+          variant="standard"
+          sx={sxTextField}
+          onChange={(e) => setUsername(e.target.value)}
+          error={!!usernameError}
+          helperText={usernameError}
+        />
 
-      {ResponseSuccess && (
-        <div className="text-black flex justify-center text-m mt-4 p-2 bg-green-400 rounded">
-          {ResponseSuccess}
-        </div>
-      )}
-    </Box>
+        <TextField
+          required
+          id="password"
+          label="Password"
+          variant="standard"
+          sx={sxTextField}
+          type={showPassword ? "text" : "password"}
+          onChange={(e) => setPassword(e.target.value)}
+          error={!!passwordError}
+          helperText={passwordError}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          required
+          id="confirm-password"
+          label="Confirm Password"
+          variant="standard"
+          sx={sxTextField}
+          type={showConfirmPassword ? "text" : "password"}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          error={!!confirmPasswordError}
+          helperText={confirmPasswordError}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowConfirmPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button variant="contained" onClick={handleSubmit} sx={{ mt: 5 }}>
+          Sign Up
+        </Button>
+
+        {ResponseError && (
+          <div className="text-black flex justify-center text-m mt-4 p-2 bg-red-400 rounded">
+            <WarningIcon />
+            {ResponseError}
+          </div>
+        )}
+
+        {ResponseSuccess && (
+          <div className="text-black flex justify-center text-m mt-4 p-2 bg-green-400 rounded">
+            {ResponseSuccess}
+          </div>
+        )}
+      </Box>
+    </div>
   );
 }
 
-export default Signup;
+export default SignupPage;
