c4c5f4388176d02324956864c4e11752d930fc77
Kyytox <kytox.dev@protonmail.com>
2025-08-23 06:38:49 +0200

    updsearch bar css

--- website/frontend/src/components/SearchBar.jsx
+++ website/frontend/src/components/SearchBar.jsx
@@ -1,9 +1,10 @@
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "./AuthContext";
-import { getAxios } from "./UtilsAxios";
 
 import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { Search } from "@mui/icons-material";
 
 function SearchBar() {
   const navigate = useNavigate();
@@ -15,20 +16,44 @@ function SearchBar() {
     }
   };
 
+  // #test if search is not empty
+  const SearchClick = () => {
+    if (search !== "") {
+      navigate("/search/" + search);
+    }
+  };
+
   return (
-    <TextField
-      id="outlined-basic"
-      className="searchBar"
-      label="Search"
-      variant="outlined"
-      size="small"
-      value={search}
-      onChange={(event) => setSearch(event.target.value)}
-      onKeyDown={handleSearch}
+    <Box
       sx={{
         margin: "0 50px 0 20px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
-    />
+    >
+      <TextField
+        id="outlined-basic"
+        className="searchBar"
+        label="Search"
+        variant="outlined"
+        size="small"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+        onKeyDown={handleSearch}
+      />
+      <Button
+        size="medium"
+        onClick={SearchClick}
+        sx={{ minWidth: "0px", paddingLeft: "10px", paddingRight: "5px" }}
+      >
+        <Search
+          sx={{
+            color: "white",
+          }}
+        />
+      </Button>
+    </Box>
   );
 }
 
