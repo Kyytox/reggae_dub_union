3290b5829cb74db4a570d9d1608568d43ab58fa5
Kyytox <kytox.dev@protonmail.com>
2025-08-23 06:40:13 +0200

    upd navba css

--- website/frontend/src/components/Navbar.jsx
+++ website/frontend/src/components/Navbar.jsx
@@ -1,25 +1,24 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { AuthContext } from "./AuthContext";
-import SearchBar from "./SearchBar";
+import { AuthContext } from "../components/AuthContext";
+import SearchBar from "../components/SearchBar";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import "./Navbar.css";
 
 function Navbar() {
   const { isLoggedIn, logout } = useContext(AuthContext);
 
   return (
-    <div className="navbar p-1 pr-10 pl-10 mb-3 w-full flex flex-wrap items-center justify-center md:justify-between">
+    <div className="navbar z-50 fixed p-1 pr-5 pl-5 mb-3 w-full flex flex-wrap items-center justify-center md:justify-between">
       <div className="navbar_logo flex justify-center w-full md:w-auto">
         <Link to="/">
           <img
-            className="navbar_logo mr-4 ml-3 md:ml-0"
+            className="navbar_logo mr-3 ml-2 md:ml-0"
             src="/logo.png"
             alt="logo"
             width="50px"
           />
         </Link>
-        <a
+        {/*<a
           href="https://www.facebook.com/association.tunguska/"
           target="_blank"
           style={{
@@ -30,7 +29,7 @@ function Navbar() {
           }}
         >
           Created by <br /> Tunguska Sound System
-        </a>
+        </a>*/}
       </div>
       <div className="navbar-items flex flex-wrap items-center justify-center">
         <Link to="/">Home</Link>
