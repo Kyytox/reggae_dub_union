c5cc25e7108fbe72fef04d3b36279cd457104752
Kyytox <kytox.dev@protonmail.com>
2025-08-25 23:33:36 +0200

    update section filter

--- website/frontend/src/components/SectionFilter.jsx
+++ website/frontend/src/components/SectionFilter.jsx
@@ -1,17 +1,37 @@
+import * as React from "react";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
 import "../App.css";
 
 function SectionFilter({
-  lstItems,
-  lstItemsSelected,
-  setLstItemsSelected,
-  sectionName,
+  fetchData,
+  lstShops,
+  lstShopsSelected,
+  setLstShopsSelected,
+  lstFormatVinyls,
+  lstFormatVinylsSelected,
+  setLstFormatVinylsSelected,
 }) {
-  // change the list of selected
-  const changeSelectedItems = (val) => {
-    // const val = sectionName === "Shops" ? item.shop_id : item;
+  const [value, setValue] = React.useState("DateAdd");
 
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  // change the list of selected
+  const changeSelectedItems = (val, sectionName) => {
+    let lstItemsSelected, setLstItemsSelected;
+    if (sectionName === "Shops") {
+      lstItemsSelected = lstShopsSelected;
+      setLstItemsSelected = setLstShopsSelected;
+    } else {
+      lstItemsSelected = lstFormatVinylsSelected;
+      setLstItemsSelected = setLstFormatVinylsSelected;
+    }
     if (lstItemsSelected.includes(val)) {
       const newArray = lstItemsSelected.filter((e) => e !== val);
       setLstItemsSelected(newArray);
@@ -22,33 +42,94 @@ function SectionFilter({
   };
 
   return (
-    <Box sx={{ "& button": { m: 0.5 } }} className="container-lstSection">
-      {lstItems.map((item) => {
-        const value = sectionName === "Shops" ? item.shop_id : item;
-        const displayText = sectionName === "Shops" ? item.shop_name : item;
+    <div className="container-sectionFilter mt-4 mb-4 flex flex-column items-center justify-center gap-6">
+      <Box
+        sx={{
+          fontWeight: "bold",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "end",
+        }}
+      >
+        <Box sx={{ "& button": { m: 0.5 } }} className="container-lstSection">
+          {lstFormatVinyls.map((item) => {
+            const value = item;
+            const displayText = item;
 
-        // is the item selected
-        const isSelected = lstItemsSelected.includes(value);
+            // is the item selected
+            const isSelected = lstFormatVinylsSelected.includes(value);
 
-        // if no item selected, all are selected (variant contained)
-        const variant =
-          lstItemsSelected.length === 0 || isSelected
-            ? "contained"
-            : "outlined";
+            // if no item selected, all are selected (variant contained)
+            const variant =
+              lstFormatVinylsSelected.length === 0 || isSelected
+                ? "contained"
+                : "outlined";
+            return (
+              <Button
+                key={value}
+                variant={variant}
+                onClick={() => changeSelectedItems(value, "Formats")}
+                size="small"
+                color="primary"
+              >
+                {displayText}
+              </Button>
+            );
+          })}
+        </Box>
+        <Box sx={{ "& button": { m: 0.5 } }} className="container-lstSection">
+          {lstShops.map((item) => {
+            const value = item.shop_id;
+            const displayText = item.shop_name;
 
-        return (
-          <Button
-            key={value}
-            variant={variant}
-            onClick={() => changeSelectedItems(value)}
-            size="small"
-            color="primary"
-          >
-            {displayText}
-          </Button>
-        );
-      })}
-    </Box>
+            // is the item selected
+            const isSelected = lstShopsSelected.includes(value);
+
+            // if no item selected, all are selected (variant contained)
+            const variant =
+              lstShopsSelected.length === 0 || isSelected
+                ? "contained"
+                : "outlined";
+            return (
+              <Button
+                key={value}
+                variant={variant}
+                onClick={() => changeSelectedItems(value, "Shops")}
+                size="small"
+                color="primary"
+              >
+                {displayText}
+              </Button>
+            );
+          })}
+        </Box>
+      </Box>
+
+      <FormControl>
+        <RadioGroup
+          aria-labelledby="controlled-radio-buttons-group"
+          name="controlled-radio-buttons-group"
+          value={value}
+          onChange={handleChange}
+        >
+          <FormControlLabel
+            value="DateAdd"
+            control={<Radio />}
+            label="Date Added"
+          />
+          <FormControlLabel value="Random" control={<Radio />} label="Random" />
+        </RadioGroup>
+      </FormControl>
+
+      <Button
+        variant="text"
+        onClick={() => {
+          fetchData();
+        }}
+      >
+        Apply Filters
+      </Button>
+    </div>
   );
 }
 
--- website/frontend/src/pages/HomePage.jsx
+++ website/frontend/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import AudioPlayer from "../components/AudioPlayer";
 import SectionFilter from "../components/SectionFilter";
 import useVinylsData from "../requests/UseVinylsData";
+import SectionPagination from "../components/SectionPagination";
 
 import "../App.css";
 
 function Home() {
   const {
+    fetchData,
     lstVinylsSelected,
     lstSongsSelected,
     lstShops,
@@ -18,22 +20,35 @@ function Home() {
     setLstFavoris,
     loadMoreData,
     topLoadMore,
+    clickChangePage,
+    nbPages,
   } = useVinylsData("home");
+  // <SectionFilter
+  //   lstItems={lstShops}
+  //   lstItemsSelected={lstShopsSelected}
+  //   setLstItemsSelected={setLstShopsSelected}
+  //   sectionName={"Shops"}
+  // />
+  // <SectionFilter
+  //   lstItems={lstFormatVinyls}
+  //   lstItemsSelected={lstFormatVinylsSelected}
+  //   setLstItemsSelected={setLstFormatVinylsSelected}
+  //   sectionName={"Formats"}
+  // />
 
+  console.log(nbPages);
   return (
     <div className="main-content">
       <SectionFilter
-        lstItems={lstShops}
-        lstItemsSelected={lstShopsSelected}
-        setLstItemsSelected={setLstShopsSelected}
-        sectionName={"Shops"}
+        fetchData={fetchData}
+        lstShops={lstShops}
+        lstShopsSelected={lstShopsSelected}
+        setLstShopsSelected={setLstShopsSelected}
+        lstFormatVinyls={lstFormatVinyls}
+        lstFormatVinylsSelected={lstFormatVinylsSelected}
+        setLstFormatVinylsSelected={setLstFormatVinylsSelected}
       />
-      <SectionFilter
-        lstItems={lstFormatVinyls}
-        lstItemsSelected={lstFormatVinylsSelected}
-        setLstItemsSelected={setLstFormatVinylsSelected}
-        sectionName={"Formats"}
-      />
+      <SectionPagination nbPages={nbPages} clickChangePage={clickChangePage} />
       <AudioPlayer
         lstSongs={lstSongsSelected}
         lstVinyls={lstVinylsSelected}
