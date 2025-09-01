610de6545cd9e73cf966063b21d6ba8fd9d57c5b
Kyytox <kytox.dev@protonmail.com>
2025-09-01 09:21:04 +0200

    rm shop in format result query

--- website/backend/utils/libs.py
+++ website/backend/utils/libs.py
@@ -25,7 +25,7 @@ def format_return_data(df):
 
     # create a DataFrame with the vinyls data
     df_vinyls = (
-        df[cols_vinyls + ["shop_id", "shop_name", "id_elem"]]
+        df[cols_vinyls + ["shop_id", "shop_name", "shop_real_name", "id_elem"]]
         .copy()
         .drop_duplicates(subset=["vinyl_id"])
     )
@@ -40,11 +40,11 @@ def format_return_data(df):
     ]
     df_songs = df[cols_songs + ["vinyl_id", "vinyl_title"]].copy()
 
-    #
-    #
-    # kepp cols for shops
-    cols_shops = ["shop_id", "shop_name"]
-    df_shops = df[cols_shops].copy().drop_duplicates()
+    # #
+    # #
+    # # kepp cols for shops
+    # cols_shops = ["shop_id", "shop_name"]
+    # df_shops = df[cols_shops].copy().drop_duplicates()
 
     # get favoris if user_id and favori_id exist
     if "user_id" in df.columns and "favori_id" in df.columns:
@@ -55,7 +55,7 @@ def format_return_data(df):
     # convert to list of dicts
     df_vinyls = df_vinyls.to_dict(orient="records")
     df_songs = df_songs.to_dict(orient="records")
-    df_shops = df_shops.to_dict(orient="records")
+    # df_shops = df_shops.to_dict(orient="records")
     df_favoris = (
         df_favoris.to_dict(orient="records") if df_favoris is not None else None
     )
@@ -64,6 +64,6 @@ def format_return_data(df):
     return {
         "vinyls": df_vinyls,
         "songs": df_songs,
-        "shops": df_shops,
+        # "shops": df_shops,
         "favoris": df_favoris,
     }
