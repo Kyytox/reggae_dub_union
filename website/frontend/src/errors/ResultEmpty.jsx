247ce896acb4d44773bda0c5d0718a62968ba7ba
Kyytox <kytox.dev@protonmail.com>
2025-09-01 09:22:28 +0200

    upd resultEmpty component

--- website/frontend/src/errors/ResultEmpty.jsx
+++ website/frontend/src/errors/ResultEmpty.jsx
@@ -1,12 +1,21 @@
+import HeadPage from "../components/HeadPage";
+import Box from "@mui/material/Box";
+import "../App.css";
+
 // composant for Result Empty
 function ResultEmpty() {
   return (
-    <div className="result-empty" style={{ marginTop: "5em" }}>
-      <h2>No Results Found</h2>
-      <p>Sorry, we couldn't find any results for your search.</p>
-      <p>Please try a different search term or check your spelling.</p>
-      <a href="/">Go to Home</a>
-    </div>
+    <Box className="main-content-noresults">
+      <HeadPage
+        text={"No Results Found for your search... "}
+        totalVinyls={null}
+      />
+
+      <Box
+        className="result-empty"
+        style={{ marginTop: "5em", width: "100%", textAlign: "center" }}
+      ></Box>
+    </Box>
   );
 }
 
