af1ee79d0511db99e45ee09f38cee84d37063ff5
Kyytox <kytox.dev@protonmail.com>
2025-09-14 12:21:33 +0200

    upd name conda env

--- website/backend/utils/config.py
+++ website/backend/utils/config.py
@@ -8,13 +8,13 @@ class Config:
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://root:root@localhost/reggae_dub_union"
+    SQLALCHEMY_DATABASE_URI = "postgresql://root:root@localhost/reg_dub_union_db"
     # SQLALCHEMY_DATABASE_URI = os.getenv("DEVELOPMENT_DATABASE_URL")
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://root:root@localhost/reggae_dub_union"
+    SQLALCHEMY_DATABASE_URI = "postgresql://root:root@localhost/reg_dub_union_db"
     # SQLALCHEMY_DATABASE_URI = os.getenv("PRODUCTION_DATABASE_URL")
 
 
