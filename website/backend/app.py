27dedf1da1d675d7040cbd00346e57c4a489e22b
Kyytox <kytox.dev@protonmail.com>
2025-09-15 19:09:12 +0200

    upd import

--- website/backend/app.py
+++ website/backend/app.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
 # from flask_migrate import Migrate
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask
 
 from flask_cors import CORS
 from utils.config import config
